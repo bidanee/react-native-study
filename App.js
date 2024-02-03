022323c7647868c4f275d1b39d08859b752868b7
bidanee <dleksql119@gmail.com>
2024-02-04 00:16:44 +0900

    💄 weatherApp 기본 UI

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>된건가?</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>JEJU</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator="false"
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>18</Text>
+          <Text style={styles.description}>Cloudy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>20</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>24</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { flex: 1, backgroundColor: "#99ccff" },
+  city: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "teal",
+    fontSize: 70,
+    fontWeight: "600",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 30,
+    fontSize: 180,
+    color: "teal",
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 50,
+    color: "teal",
   },
 });
