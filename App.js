23565288a906620417b2a97f262fefaa414939c6
bidanee <dleksql119@gmail.com>
2024-02-04 18:03:24 +0900

    ✨ location api 사용

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    const permission = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("위치에 접근할 수 없습니다.");
+        return;
+      }
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      const location = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+      setCity([location[0].city, location[0].district]);
+    };
+    permission();
+  }, []);
+  console.log(city);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>JEJU</Text>
+        <Text style={styles.cityName}>{city[0]}</Text>
+        <Text style={styles.districtName}>{city[1]}</Text>
       </View>
       <ScrollView
         horizontal
@@ -44,7 +68,13 @@ const styles = StyleSheet.create({
   },
   cityName: {
     color: "teal",
-    fontSize: 70,
+    fontSize: 50,
+    fontWeight: "500",
+  },
+  districtName: {
+    marginTop: 20,
+    color: "teal",
+    fontSize: 50,
     fontWeight: "600",
   },
   weather: {},
@@ -53,7 +83,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   temp: {
-    marginTop: 30,
     fontSize: 180,
     color: "teal",
   },
